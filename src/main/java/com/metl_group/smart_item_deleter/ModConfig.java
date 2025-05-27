673ae8897e437326721c7d863e4cdd51d9821dda
David Brückhändler <davidundivan.99@gmail.com>
2025-05-27 16:49:27 +0200

    Made Keep Percent available through Config File

--- src/main/java/com/metl_group/smart_item_deleter/ModConfig.java
+++ src/main/java/com/metl_group/smart_item_deleter/ModConfig.java
@@ -1,7 +1,7 @@
 package com.metl_group.smart_item_deleter;
 
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
@@ -10,8 +10,12 @@ public class ModConfig {
 
     public static final ModConfigSpec SPEC;
     public static ModConfigSpec.IntValue THRESHOLD;
+    public static ModConfigSpec.IntValue KEEP_PERCENT;
+    public static ModConfigSpec.BooleanValue DEBUG;
 
     public static int thresholdCached = 16000; // Default
+    public static int keepPercentCached = 50;
+    public static boolean debugCached = false;
 
     static {
         ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
@@ -20,6 +24,14 @@ public class ModConfig {
         THRESHOLD = builder
                 .comment("Maximale Anzahl von ItemEntities bevor gelöscht wird")
                 .defineInRange("threshold", 16000, 100, 100000);
+
+        KEEP_PERCENT = builder
+                .comment("Prozentsatz der Entities, die nach Cleanup erhalten bleiben (0–100)")
+                .defineInRange("keep_percent", 50, 0, 100);
+
+        DEBUG = builder
+                .comment("ob debug per chat ausgegeben wird")
+                .define("debug", false);
 
         builder.pop();
         SPEC = builder.build();
@@ -32,6 +44,8 @@ public class ModConfig {
         if (!event.getConfig().getSpec().equals(ModConfig.SPEC)) return;
 
         thresholdCached = ModConfig.THRESHOLD.get(); // Jetzt ist es sicher!
+        keepPercentCached = ModConfig.KEEP_PERCENT.get();
+        debugCached = ModConfig.DEBUG.get();
         LOGGER.info("Threshold config loaded: {}", thresholdCached);
     }
 }
